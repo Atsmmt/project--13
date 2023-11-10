9ab32895fa2bfe2e55c39deb6f828600e5aea992
Tabassum Rizvi <tabassum.rizvi@hotmail.com>
2023-11-10 14:06:10 +0000

    Initial commit with challenge 1 complete.

---
+++ LeetCode/LeetCodeProblem-1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    for (let a = 0; a < nums.length; a++)
+    {
+        for (let b = a+1; b < nums.length; b++)
+        {
+            if (target-nums[a] === nums[b])
+            {
+                return [a,b]
+            }
+        }
+    }
+    
+};
